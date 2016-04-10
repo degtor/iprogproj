69531e470dd6c3276361ce08299209717352bf53
Ariel Blomkvist Rova <arielr@kth.se>
2016-04-10 14:09:09 +0200

    Merge, sondag 10/4 efter screencast

--- app/app.js
+++ app/app.js
@@ -1,48 +1,16 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-<<<<<<< HEAD
-var ideaPlanner = angular.module('myApp', ['ngRoute', 'duScroll', 'angular-venn']);
+var ideaPlanner = angular.module('myApp', ['ngRoute', 'duScroll', 'firebase', 'chart.js', 'angular-venn']);
 
-ideaPlanner.config(['$routeProvider', function($routeProvider) {
-
-  $routeProvider.
-  when('/home', {
-    templateUrl: 'partials/homeView/home.html'
-  }).
-  when('/fullpage', {
-      templateUrl: 'partials/fullpageView/fullpage.html',
-      //controller: 'FullpageCtrl'
-    }).
-    /*when('/problem', {
-      templateUrl: 'partials/fullpageView/fullpage.html#problem',
-    }).
-    when('/dinneroverview', {
-      templateUrl: 'partials/dinneroverview.html',
-      controller: 'OverviewCtrl'
-    }).
-    when('/preparation', {
-      templateUrl: 'partials/preparation.html',
-      controller: 'PreparationCtrl'
-    }).*/
-=======
-var ideaPlanner = angular.module('myApp', ['ngRoute', 'duScroll', 'firebase', 'chart.js']);
-
->>>>>>> d7764730a3934385fa6d79314f5b571538bb271f
 
 ideaPlanner.config(['$routeProvider', function($routeProvider) {
 
   $routeProvider.when('/home', {
-    templateUrl: 'partials/homeView/home.html'
-  })
-      .when('/fullpage', {
-        templateUrl: 'partials/fullpageView/fullpage.html'
-  })
+      templateUrl: 'partials/homeView/home.html'
+    })
+    .when('/fullpage', {
+      templateUrl: 'partials/fullpageView/fullpage.html'
+    })
 
-<<<<<<< HEAD
-  otherwise({ redirectTo: '/home' });
+  .otherwise({ redirectTo: '/home' });
 }]);
-=======
-      .otherwise({redirectTo: '/home'});
-}]);
->>>>>>> d7764730a3934385fa6d79314f5b571538bb271f
