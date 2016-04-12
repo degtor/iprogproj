e3fb483b425bc057ffd49a950aca6f40802c2820
Emma Klint <eklint@kth.se>
2016-04-12 19:03:09 +0200

    no message

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var ideaPlanner = angular.module('myApp', ['ngRoute', 'duScroll']);
+var ideaPlanner = angular.module('myApp', ['ngRoute', 'duScroll', 'chart.js']);
 
 ideaPlanner.config(['$routeProvider', function($routeProvider) {
 
@@ -13,17 +13,22 @@ ideaPlanner.config(['$routeProvider', function($routeProvider) {
       templateUrl: 'partials/fullpageView/fullpage.html',
       //controller: 'FullpageCtrl'
     }).
-    /*when('/problem', {
+    when('/problem', {
       templateUrl: 'partials/fullpageView/fullpage.html#problem',
+      controller: 'ProblemCtrl'
     }).
-    when('/dinneroverview', {
-      templateUrl: 'partials/dinneroverview.html',
-      controller: 'OverviewCtrl'
+    when('/title', {
+      templateUrl: 'partials/fullpageView/fullpage.html#title',
+      controller: 'TitleCtrl'
+    }).
+    when('/market', {
+      templateUrl: 'partials/fullpageView/fullpage.html#market',
+      controller: 'marketCtrl'
     }).
     when('/preparation', {
       templateUrl: 'partials/preparation.html',
       controller: 'PreparationCtrl'
-    }).*/
+    }).
 
 
 
